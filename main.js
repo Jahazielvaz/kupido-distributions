0a98e80d2eb0cba18bb31899a66f4fa622c61206
Jahaziel.vaz <jahaziel.vaz@gmail.com>
2018-01-07 18:47:07 -0700

    New Header, working on footer

---
+++ main.js
@@ -0,0 +1,17 @@
+// var scheme = document.getElementById("scheme");
+// var colSection = document.getElementById("colors");
+// var col1 = document.getElementById("div1");
+// var col2 = document.getElementById("div2");
+// var col3 = document.getElementById("div3");
+// var col4 = document.getElementById("div4");
+// var col5 = document.getElementById("div5");
+// var col6 = document.getElementById("div6");
+// var col7 = document.getElementById("div7");
+// var cols = [col1, col2, col3, col4, col5, col6, col7];
+// var colorCont = document.getElementById("colCont")
+
+// var headerNavList = document.querySelector("#header-nav li");
+//
+// headerNavList.addEventListener("mouseover", function(){
+//   headerNavList.style.background = "#314770";
+// });
